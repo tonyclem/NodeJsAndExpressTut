82df834d5b9518ada18c874c9ce070e687535aa2
tonyclem <tonyclem001@gmail.com>
2022-04-13 23:06:45 +0200

    added postman api, get, post lesson, and put as well

--- 2-express-tut/app.js
+++ 2-express-tut/app.js
@@ -18,7 +18,24 @@ app.get("/api/people", (req, res) => {
 });
 // post the new person request and open a new page /api/people
 app.post("/api/people", (req, res) => {
-  res.status(201).send("Successfully");
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide name value" });
+  }
+  res.status(201).json({ success: true, person: name });
+});
+
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide name value" });
+  }
+  // to sent data to postman
+  res.status(201).send({ success: true, data: [...people, name] });
 });
 
 // post request and response to  new page /login
@@ -28,6 +45,26 @@ app.post("/login", (req, res) => {
     return res.status(200).send(`Welcome ${name}`);
   }
   res.status(401).send("Please Provide Credential"); // send back a response
+});
+
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `No person found with is id ${id}` });
+  }
+
+  const newPerson = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+
+  res.status(200).json({ success: true, data: newPerson });
 });
 
 app.listen(5000, () => {

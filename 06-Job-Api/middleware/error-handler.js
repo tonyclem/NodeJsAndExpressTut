e7f1293704ef69d33d6cce406b02cbb1ea90f9b9
tonyclem <tonyclem001@gmail.com>
2022-04-19 22:44:56 +0200

    modified the Cast Error login

--- 06-Job-Api/middleware/error-handler.js
+++ 06-Job-Api/middleware/error-handler.js
@@ -25,6 +25,13 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     )} field, please chooses another value`;
     customError.statusCode = 400;
   }
+
+  // if error is CastError
+  if (err.name === "CastError") {
+    customError.message = `No item found with is id: ${err.value}`;
+    customError.statusCode = 404;
+  }
+
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ msg: customError.message });
 };

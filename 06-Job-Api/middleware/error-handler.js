ae850fbee72b2bad825f462a3591c369962e1962
tonyclem <tonyclem001@gmail.com>
2022-04-19 22:09:13 +0200

    added error logic using StatusCodes

--- 06-Job-Api/middleware/error-handler.js
+++ 06-Job-Api/middleware/error-handler.js
@@ -1,10 +1,25 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 
-module.exports = errorHandlerMiddleware
+// error handler
+const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || "Something went wrong try again later",
+  };
+
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ msg: err.message });
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.message = `Duplicate values entered for ${Object.keys(
+      err.keyValue
+    )} field, please chooses another value`;
+    customError.statusCode = 400;
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({ msg: customError.message });
+};
+
+module.exports = errorHandlerMiddleware;

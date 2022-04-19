12ea944d6ebdc8db5ec2a4d4774101f4976816e4
tonyclem <tonyclem001@gmail.com>
2022-04-19 23:04:58 +0200

    added extra security built-in

--- 06-Job-Api/app.js
+++ 06-Job-Api/app.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
+
+// Extra Security Package
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 const authenticateUser = require("./middleware/authentication");
@@ -12,8 +19,21 @@ const jobRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+// https://www.npmjs.com/package/express-rate-limit // read more about this package
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
 // extra packages
+
+// Security Package
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // connect to the database
 const connectDB = require("./db/connect");

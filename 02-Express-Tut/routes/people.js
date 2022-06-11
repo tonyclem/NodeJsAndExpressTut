08323d6be7b169d60033d3cecced669435cef2e3
tonyclem <tonyclem001@gmail.com>
2022-06-11 23:10:01 +0200

    adding deference function or method doing some thing to get and display data

--- 02-Express-Tut/routes/people.js
+++ 02-Express-Tut/routes/people.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 let { people } = require("../data");
 
-// const {
-//   getPeople,
-//   createPerson,
-//   createPersonPostman,
-//   updatePerson,
-//   deletePerson,
-// } = require("../controllers/people");
+const {
+  getPeople,
+  createPerson,
+  createPersonPostman,
+  updatePerson,
+  deletePerson,
+} = require("../controllers/people");
 
 // Get all people
 // router.get("/", getPeople);
@@ -23,68 +23,69 @@ let { people } = require("../data");
 // // Delete a person by id and return the updated list
 // router.delete("/:id", deletePerson);
 
-// chain the routes method
-// router.route("/").get(getPeople).post(createPerson);
-// router.route("/postman").post(createPersonPostman);
-// router.route("/:id").put(updatePerson).delete(deletePerson);
-
-// module.exports = router;
-
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
-
-router.post("/", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "please provide name value" });
-  }
-  res.status(201).json({ success: true, person: name });
-});
-
-router.post("/postman", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "please provide name value" });
-  }
-  res.status(201).json({ success: true, data: [...people, name] });
-});
-
-router.put("/:id", (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-
-  const person = people.find((person) => person.id === Number(id));
-
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${id}` });
-  }
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name;
-    }
-    return person;
-  });
-  res.status(200).json({ success: true, data: newPeople });
-});
-
-router.delete("/:id", (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.id));
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${req.params.id}` });
-  }
-  const newPeople = people.filter(
-    (person) => person.id !== Number(req.params.id)
-  );
-  return res.status(200).json({ success: true, data: newPeople });
-});
+// // chain the routes method
+router.route("/").get(getPeople).post(createPerson);
+router.route("/postman").post(createPersonPostman);
+router.route("/:id").put(updatePerson).delete(deletePerson);
 
 module.exports = router;
+
+// same as above function
+// router.get("/", (req, res) => {
+//   res.status(200).json({ success: true, data: people });
+// });
+
+// router.post("/", (req, res) => {
+//   const { name } = req.body;
+//   if (!name) {
+//     return res
+//       .status(400)
+//       .json({ success: false, msg: "please provide name value" });
+//   }
+//   res.status(201).json({ success: true, person: name });
+// });
+
+// router.post("/postman", (req, res) => {
+//   const { name } = req.body;
+//   if (!name) {
+//     return res
+//       .status(400)
+//       .json({ success: false, msg: "please provide name value" });
+//   }
+//   res.status(201).json({ success: true, data: [...people, name] });
+// });
+
+// router.put("/:id", (req, res) => {
+//   const { id } = req.params;
+//   const { name } = req.body;
+
+//   const person = people.find((person) => person.id === Number(id));
+
+//   if (!person) {
+//     return res
+//       .status(404)
+//       .json({ success: false, msg: `no person with id ${id}` });
+//   }
+//   const newPeople = people.map((person) => {
+//     if (person.id === Number(id)) {
+//       person.name = name;
+//     }
+//     return person;
+//   });
+//   res.status(200).json({ success: true, data: newPeople });
+// });
+
+// router.delete("/:id", (req, res) => {
+//   const person = people.find((person) => person.id === Number(req.params.id));
+//   if (!person) {
+//     return res
+//       .status(404)
+//       .json({ success: false, msg: `no person with id ${req.params.id}` });
+//   }
+//   const newPeople = people.filter(
+//     (person) => person.id !== Number(req.params.id)
+//   );
+//   return res.status(200).json({ success: true, data: newPeople });
+// });
+
+// module.exports = router;

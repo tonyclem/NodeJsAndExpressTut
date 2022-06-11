20355998338a3f1163e5960c332d9e53e01feb08
tonyclem <tonyclem001@gmail.com>
2022-06-11 21:32:05 +0200

    adding post required to data

--- 02-Express-Tut/app.js
+++ 02-Express-Tut/app.js
@@ -8,9 +8,20 @@ app.use(express.static("./methods-public"));
 // parse form data
 app.use(express.urlencoded({ extended: false }));
 // parse json
+app.use(express.json());
 
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  }
+  res.status(201).json({ success: true, person: name });
 });
 
 app.post("/login", (req, res) => {
@@ -18,10 +29,9 @@ app.post("/login", (req, res) => {
   if (name) {
     return res.status(200).send(`Welcome ${name}`);
   }
-  console.log(req.body);
-  res.status(401).send("POST CLEMENT");
+  res.status(401).send("POST ERROR");
 });
 
 app.listen(8080, () => {
-  console.log("hello king of billionarie the Server is running at port 8080");
+  console.log("Server is running at port 8080");
 });

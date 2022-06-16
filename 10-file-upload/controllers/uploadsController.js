ba35babe1c85f9839464410733dd15ab44610f17
tonyclem <tonyclem001@gmail.com>
2022-06-16 20:50:17 +0200

    adding more function, to get image and updload it, as well as testing code for the postman

---
+++ 10-file-upload/controllers/uploadsController.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
+const uploadProductImage = async (req, res) => {
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No Files Uploaded");
+  }
+
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please Upload Image");
+  }
+
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError("Please uploaded image smaller 1MB");
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+module.exports = {
+  uploadProductImage,
+};

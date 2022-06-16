ca93449d2754c4730224470604c40ed621621bcc
tonyclem <tonyclem001@gmail.com>
2022-06-16 22:55:19 +0200

    Basic Routes and middleware

--- 09-E-Commerce-api/app.js
+++ 09-E-Commerce-api/app.js
@@ -1,12 +1,30 @@
 require("dotenv").config();
+require("express-async-errors");
 
+// Express
 const express = require("express");
 const app = express();
 
+// Middleware
+app.use(express.json());
 // connectDB
 const connectDB = require("./db/connect");
 
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to E-commerce api</h1>");
+});
+
+// Error handle
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+// port
 const port = process.env.PORT || 3000;
+
+// start server
 
 const start = async () => {
   try {

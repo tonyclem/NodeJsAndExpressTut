46859e57ec5834567794cc328ca381a4c54295fc
tonyclem <tonyclem001@gmail.com>
2022-06-16 21:50:50 +0200

    adding cloudinary method and uploading image to cloud

--- 10-file-upload/controllers/uploadsController.js
+++ 10-file-upload/controllers/uploadsController.js
@@ -1,8 +1,10 @@
 const path = require("path");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
-const uploadProductImage = async (req, res) => {
+const uploadProductImageLocal = async (req, res) => {
   if (!req.files) {
     throw new CustomError.BadRequestError("No Files Uploaded");
   }
@@ -28,6 +30,20 @@ const uploadProductImage = async (req, res) => {
   return res
     .status(StatusCodes.OK)
     .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: "file-upload",
+    }
+  );
+
+  fs.unlinkSync(req.files.image.tempFilePath);
+
+  return res.status(StatusCodes.OK).json({ src: result.secure_url });
 };
 
 module.exports = {

3693048a0be119cd66ac19ed99ec2fb430d1a7f6
tonyclem <tonyclem001@gmail.com>
2022-06-17 11:26:19 +0200

    setup alternative for admin setup

--- 09-E-Commerce-api/controllers/authController.js
+++ 09-E-Commerce-api/controllers/authController.js
@@ -3,13 +3,13 @@ const User = require("../models/User");
 const CustomAPIError = require("../errors");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, name, password } = req.body;
   const emailAlreadyExists = await User.findOne({ email });
   if (emailAlreadyExists) {
     throw new CustomAPIError.BadRequestError("Email Already exists");
   }
 
-  const user = await User.create(req.body);
+  const user = await User.create({ email, name, password, role });
   res.status(StatusCodes.CREATED).json({ user });
 };
 

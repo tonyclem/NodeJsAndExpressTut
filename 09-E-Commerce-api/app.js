10ab5bc7dcc3a6bef229dec79c6ca819214b9c49
tonyclem <tonyclem001@gmail.com>
2022-06-17 15:05:16 +0200

    done with the cookie setup

--- 09-E-Commerce-api/app.js
+++ 09-E-Commerce-api/app.js
@@ -20,10 +20,10 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(morgan("tiny"));
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser(process.env.JWT_SECRET));
 
 app.get("/api/v1", (req, res) => {
-  console.log(req.cookies);
+  console.log(req.signedCookies);
   res.send("<h1>Welcome to E-commerce Api</h1>");
 });
 app.get("/", (req, res) => {

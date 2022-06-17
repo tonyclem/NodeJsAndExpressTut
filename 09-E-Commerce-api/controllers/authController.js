cdd46180a4f8eae1c2dd9a5946b3585949b5e18a
tonyclem <tonyclem001@gmail.com>
2022-06-17 15:39:14 +0200

    adding login Route and testing with postman

--- 09-E-Commerce-api/controllers/authController.js
+++ 09-E-Commerce-api/controllers/authController.js
@@ -22,7 +22,26 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("Hello login");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new CustomAPIError.BadRequestError(
+      "Please provide email and password"
+    );
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new CustomAPIError.UnauthenticatedError("Invalid Credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomAPIError.UnauthenticatedError("Invalid Credentials");
+  }
+
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
 const logout = async (req, res) => {

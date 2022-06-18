51af913500b8d8cef619aa36ded4ac5b376d6e4c
tonyclem <tonyclem001@gmail.com>
2022-06-18 20:46:13 +0200

    added review model layout with mongoose

---
+++ 09-E-Commerce-api/models/Review.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "Plesea provide rating"],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Plesea provide rating"],
+      maxlength: 100,
+    },
+    comment: {
+      type: String,
+      required: [true, "Please provide reviews text"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: "product",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+module.exports = mongoose.model("Revie", ReviewSchema);

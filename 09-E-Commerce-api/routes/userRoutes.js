9306e0a998e28b6dc0d8291e29d0ccbbe7fd440e
tonyclem <tonyclem001@gmail.com>
2022-06-18 09:39:16 +0200

    added new authorized error file to handle unauthorize user, and authentication user setup

--- 09-E-Commerce-api/routes/userRoutes.js
+++ 09-E-Commerce-api/routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { authenticateUser } = require("../middleware/authentication");
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require("../middleware/authentication");
 
 const {
   getAllUsers,
@@ -10,7 +13,9 @@ const {
   updateUserAndPassword,
 } = require("../controllers/userController");
 
-router.route("/").get(authenticateUser, getAllUsers);
+router
+  .route("/")
+  .get(authenticateUser, authorizePermissions("admin", "owner"), getAllUsers);
 
 router.route("/showMe").get(showCurrentUser);
 router.route("/updateUser").patch(updateUser);

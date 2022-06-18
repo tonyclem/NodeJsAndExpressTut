95bc4b8cfa1acc4d706800bcd37cebd1abc1f73f
tonyclem <tonyclem001@gmail.com>
2022-06-18 14:41:32 +0200

    added product Model

---
+++ 09-E-Commerce-api/models/Product.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide Name"],
+      trim: true,
+      minlength: [100, "Name can not be longer than 100 characters"],
+      maxlength: 50,
+    },
+    price: {
+      type: Number,
+      required: [true, "Product price is required"],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, "Please provide product description"],
+      maxlength: [
+        1000,
+        "Product description can not be longer than 1000 characters",
+      ],
+    },
+    image: {
+      type: String,
+      default: "/uploads/example.jpg",
+    },
+    category: {
+      type: String,
+      required: [true, "Please provide product category"],
+      enum: ["office", "kitchen", "bedroom"],
+    },
+    company: {
+      type: String,
+      required: [true, "Please provide company"],
+
+      enum: {
+        // enum is a mongoose method that allows you to set a list of values
+        values: ["ikea", "liddy", "marcos"],
+        message: "{VALUE} is not a valid company",
+      },
+    },
+    colors: {
+      type: [String],
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      default: 15,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);

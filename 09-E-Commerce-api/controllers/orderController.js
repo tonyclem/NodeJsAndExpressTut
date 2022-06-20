b17b7541ef5d1f5fa09f1fe4360bb84bdac0dd3a
tonyclem <tonyclem001@gmail.com>
2022-06-20 21:22:06 +0200

    added create order functions

--- 09-E-Commerce-api/controllers/orderController.js
+++ 09-E-Commerce-api/controllers/orderController.js
@@ -1,5 +1,15 @@
+const Order = require("../models/Order");
+const Product = require("../models/Product");
+
+const { checkPermissions } = require("../utils");
+
 const { StatusCodes } = require("http-status-codes");
 const CustomAPIError = require("../errors");
+
+const fakeStripeAPI = async ({ amount, currency }) => {
+  const client_secret = "someRandomValue";
+  return { client_secret, amount };
+};
 
 const getAllOrders = async (req, res) => {
   res.send("Get all orders");
@@ -14,7 +24,62 @@ const getCurrentUserOrders = async (req, res) => {
 };
 
 const createOrder = async (req, res) => {
-  res.send("Get all orders");
+  const { items: cartItems, tax, shippingFee } = req.body;
+  if (!cartItems || cartItems.length < 1) {
+    throw new CustomAPIError.BadRequestError("No cart items provided");
+  }
+
+  if (!tax || !shippingFee) {
+    throw new CustomAPIError.BadRequestError(
+      "Please provide tax and shipping fee"
+    );
+  }
+
+  let orderItems = [];
+  let subtotal = 0;
+
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+    if (!dbProduct) {
+      throw new CustomAPIError.NotFoundError(
+        `No product with id ${item.product}`
+      );
+    }
+    const { name, price, image, _id } = dbProduct;
+    const singleOrderItem = {
+      amount: item.amount,
+      name,
+      price,
+      image,
+      product: _id,
+    };
+    // add item to order list
+    orderItems = [...orderItems, singleOrderItem];
+    subtotal += item.amount * price;
+  }
+
+  // calculate tatal
+  const total = tax + shippingFee + subtotal;
+
+  // get client secret
+  const paymentIntent = await fakeStripeAPI({
+    amount: total,
+    currency: "usd",
+  });
+
+  const order = await Order.create({
+    orderItems,
+    total,
+    subtotal,
+    tax,
+    shippingFee,
+    clientSecret: paymentIntent.client_secret,
+    user: req.user.userId,
+  });
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({ order, clientSecret: order.clientSecret });
 };
 
 const updateOrder = async (req, res) => {
